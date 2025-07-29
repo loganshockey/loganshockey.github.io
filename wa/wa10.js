c384c4faf6e39d0c4090e496a18385d1ba6afd88
Logan Shockey <loganshockey@outlook.com>
2025-07-29 17:11:24 -0600

    Update wa10.js

--- wa/wa10.js
+++ wa/wa10.js
@@ -2,11 +2,6 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
-}
-
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
 const insertX = [ "Willy the Goblin",
@@ -24,25 +19,14 @@ const insertZ = [ "spontaneously combusted",
 "turned into a slug and crawled away"
 ];
 
-randomize.addEventListener('click', result);
 
-function result() {
-
-  if(customName.value !== '') {
-    const name = customName.value;
-
-  }
-
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
-  }
-
-  story.textContent = ;
-  story.style.visibility = 'visible';
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
 }
 
+
+function generateStory() {
 let newStory = storyText;
 
 const xItem = randomValueFromArray(insertX);
@@ -53,13 +37,12 @@ newStory = newStory.replace(/:insertx:/g, xItem);
 newStory = newStory.replace(":inserty:", yItem);
 newStory = newStory.replace(":insertz:", zItem);
 
-const customName = document.getElementById("customName").value;
+
 if (customName !== "") {
   newStory = newStory.replace("Bob", customName);
 }
 
-const uk = document.getElementById("uk").checked;
-if (uk) {
+if (document.getElementById("uk").checked) {
   const weight = Math.round(300 / 14) + " stone";
   const temperature = Math.round((94 - 32) * 5 / 9) + " centigrade";
 
@@ -67,5 +50,8 @@ if (uk) {
   newStory = newStory.replace("94 fahrenheit", temperature);
 }
 
-const story = document.getElementById("story");
-story.textContent = newStory;
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
+
+randomize.addEventListener('click', generateStory);
