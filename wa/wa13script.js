202be2e1e68fa1df6a801a4665fce55702a1c831
Logan Shockey <loganshockey@outlook.com>
2025-08-05 22:22:51 -0600

    Update wa13script.js

--- wa/wa13script.js
+++ wa/wa13script.js
@@ -2,10 +2,17 @@ let employees = [
   { firstName: "Sam", department: "Tech", designation: "Manager", salary: 40000, raiseEligible: true },
   { firstName: "Mary", department: "Finance", designation: "Trainee", salary: 18500, raiseEligible: true },
   { firstName: "Bill", department: "HR", designation: "Executive", salary: 21200, raiseEligible: false },
+  { firstName: "Anna", department: "Tech", designation: "Executive", salary: 25600, raiseEligible: false },
 ];
+
+console.log("Employees");
+console.log(employees);
 
 let company = {
   companyName: "Tech Stars",
   website: "www.techstars.site",
   employees: employees
 };
+
+console.log("Company Info");
+console.log(company);
